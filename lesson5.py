e1a222fc52577a3e61d293f0c23d5b465d5aa940
Anzhela-Elbakyan <elbakyananzhela@gmail.com>
2024-02-21 15:25:01 +0400

    Nenc Kaskacox Ylner eli

---
+++ lesson5.py
@@ -0,0 +1,63 @@
+# 1
+# calculator = lambda operation: eval(operation)
+
+# operation = input("Enter your operations\n >> ")
+# print("Result:",calculator(operation))
+
+# 2 
+# def find_max(a , b):
+#     return a if a > b else b
+
+# a = int(input('Enter first number: '))
+# b = int(input('Enter first number: '))
+
+# print(find_max(a , b))
+
+# 3
+
+# def summary(*args):
+#     s = 0
+#     for arg in args:
+#         s += arg
+
+#     return s
+
+# print(summary(1 , 2 , 3, 4 , 5))
+
+
+
+# 4
+# def multiply(*args):
+#     s = 1
+#     for arg in args:
+#         s *= arg
+
+#     return s
+
+# print(multiply(1 , 2 , 3, 4 , 5))
+
+# 5
+# def durak_sum(*args):
+#     str_args = map(str , args)
+#     result = "".join(str_args)
+#     return result
+
+# print(durak_sum(1 , 2 , 'Hello', 'fayton'))
+
+# 6
+# def function(age_1 , age_2, age_3):
+#     ages = [age_1, age_2, age_3]
+
+#     for age in ages:
+#         if age <= 16:
+#             return "Too Young Nigga!!! Get Out"
+
+#     return "Get Ready!!!!"    
+
+
+# age1 = int(input("Enter first passanger age: "))
+# age2 = int(input("Enter second passanger age: "))
+# age3 = int(input("Enter third passanger age: "))
+
+# print(function(age1 , age2, age3))
+
